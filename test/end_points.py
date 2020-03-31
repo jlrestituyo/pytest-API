2bf85461d083831cf8c6a82dd71b26cddfc5ee6a
Juan Luis Restituyo <juanluisrestituyo@gmail.com>
2020-03-31 01:36:49 -0300

    Setting up the project

---
+++ test/end_points.py
@@ -0,0 +1,14 @@
+def get_all_people_name():
+    return ""
+
+def get_people_last_name():
+    return ""
+
+
+def get_person_id_by_name():
+    return ""
+
+# People Data retreive by the end points corresponds to the following fields
+# name, last name, date_of_birth, deparment,  salary
+
+
